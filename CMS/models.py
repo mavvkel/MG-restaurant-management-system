11b03e35531a3881eac04ed1256881ca9f61734a
olasconan <olasconan1@tlen.pl>
2023-04-05 23:53:57 +0200

    I added some useless class, temporarly to test REST API commands. I also added REST to our project

--- CMS/models.py
+++ CMS/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class tempCustomer(models.Model):
+    name = models.CharField(max_length=200)
+    create = models.DateTimeField(auto_now_add=True)
