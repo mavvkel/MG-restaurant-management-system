345a97819198da2c2a2aa39de0782bbcef9013ef
Bartkosa <bart.kosieradzki@gmail.com>
2023-05-24 23:27:40 +0200

    structure changes

--- RMS/views.py
+++ RMS/views.py
@@ -11,6 +11,9 @@ def start_view(request):
 def orders_view(request):
     return render(request, 'RMS/orders.html')
 
+def tables_view(request):
+    return render(request, 'RMS/table_booking.html')
+
 
 def add_order_view(request):
     response = requests.get('http://localhost:8000/api/restaurant/menu')
@@ -20,6 +23,38 @@ def add_order_view(request):
 
 
 def menu_view(request):
+    response = requests.get('http://localhost:8000/api/restaurant/menu')
+    data = response.json()
+
+    category1 = []  # Starter
+    category2 = []  # Main course
+    category3 = []  # Soup
+    category4 = []  # Salad
+    category5 = []  # Dessert
+
+    for item in data:
+        stage = item['stage']
+        if stage == DishRestaurantMenuEntry.DishStage.STARTER:
+            category1.append(item)
+        elif stage == DishRestaurantMenuEntry.DishStage.MAIN_COURSE:
+            category2.append(item)
+        elif stage == DishRestaurantMenuEntry.DishStage.SOUP:
+            category3.append(item)
+        elif stage == DishRestaurantMenuEntry.DishStage.SALAD:
+            category4.append(item)
+        elif stage == DishRestaurantMenuEntry.DishStage.DESSERT:
+            category5.append(item)
+
+    return render(request, 'RMS/menu.html', {
+        'category1': category1,
+        'category2': category2,
+        'category3': category3,
+        'category4': category4,
+        'category5': category5
+    })
+
+
+def dish_form_view(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         price = request.POST.get('price')
@@ -39,42 +74,10 @@ def menu_view(request):
             return redirect('menu')
 
         else:
-            error_message = "An error occurred while submitting the form."
-            return render(request, 'RMS/menu.html', {'error_message': error_message})
+
+            return redirect('dish-form')
     else:
-        response = requests.get('http://localhost:8000/api/restaurant/menu')
-        data = response.json()
-
-        category1 = []  # Starter
-        category2 = []  # Main course
-        category3 = []  # Soup
-        category4 = []  # Salad
-        category5 = []  # Dessert
-
-        for item in data:
-            stage = item['stage']
-            if stage == DishRestaurantMenuEntry.DishStage.STARTER:
-                category1.append(item)
-            elif stage == DishRestaurantMenuEntry.DishStage.MAIN_COURSE:
-                category2.append(item)
-            elif stage == DishRestaurantMenuEntry.DishStage.SOUP:
-                category3.append(item)
-            elif stage == DishRestaurantMenuEntry.DishStage.SALAD:
-                category4.append(item)
-            elif stage == DishRestaurantMenuEntry.DishStage.DESSERT:
-                category5.append(item)
-
-        return render(request, 'RMS/menu.html', {
-            'category1': category1,
-            'category2': category2,
-            'category3': category3,
-            'category4': category4,
-            'category5': category5
-        })
-
-
-def dish_form_view(request):
-    return render(request, 'RMS/dish_form.html')
+        return render(request, 'RMS/dish_form.html')
 
 
 
