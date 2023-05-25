bf29270901b5f0013be8c298d432e8c12b42356a
Bartkosa <bart.kosieradzki@gmail.com>
2023-05-25 10:31:36 +0200

    changed frontend to work with new API for drink and dish

--- RMS/urls.py
+++ RMS/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
     path('add_order/', views.add_order_view, name='add-order'),
     path('dish-form/', views.dish_form_view, name='dish-form'),
+    path('drink-form/', views.drink_form_view, name='drink-form'),
     path('menu/', views.menu_view, name='menu'),
 ]
