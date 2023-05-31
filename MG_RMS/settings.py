8738be000ff1daa7e89d097fa8171d6b15eef22c
olasconan <olasconan1@tlen.pl>
2023-05-31 14:23:10 +0200

    Need to change supposed text

--- MG_RMS/settings.py
+++ MG_RMS/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ['thisshoulberandom']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
